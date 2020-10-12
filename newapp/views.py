8ef07e0be7ffcb8a0ddf450cbb488e2aca3ce14e
varuni-bit <varuni3443@gmail.com>
2020-10-12 14:21:57 +0530

    created django app

---
+++ newapp/views.py
@@ -0,0 +1,7 @@
+from django.shortcuts import render
+from .models import Movies
+
+# Create your views here.
+def movie_list(request):
+    movie_objects = Movies.objects.all()
+    return render(request,'newapp/movies_list.html',{'movie_objects':movie_objects})
