7d24d0eca86b302935940e0a7efb18679b157251
varuni-bit <varuni3443@gmail.com>
2020-10-12 21:26:27 +0530

    added a search bar for filtering and searching

--- newapp/views.py
+++ newapp/views.py
@@ -5,6 +5,11 @@ from django.core.paginator import Paginator
 # Create your views here.
 def movie_list(request):
     movie_objects = Movies.objects.all()
+    #for search bar
+    movie_name = request.GET.get('movie_name')
+    if movie_name != '' and movie_name is not None:
+        # name__icontains job is to display contents which are similar to the text entered in search bar
+        movie_objects = movie_objects.filter(name__icontains=movie_name)
     #paginator for page wgich uses 4 movies per page
     paginator = Paginator(movie_objects,3)
     #this will get the current page number
